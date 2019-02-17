d3c9639e983217dc16295a3d65a4f3dbab64a695
Silversr <huiben.zhang.silver@gmail.com>
2019-02-17 17:08:11 +1000

    GlobalException

--- Startup.cs
+++ Startup.cs
@@ -111,7 +111,7 @@ namespace DatingApp.API
                                 if (error != null)
                                 {
                                     context.Response.AddApplicationError(error.Error.Message);
-                                    //await context.Response.WriteAsync(error.Error.Message);
+                                    await context.Response.WriteAsync(error.Error.Message);
                                 }
                             }
 
